a3d8d3d19c9fd1ec380fc81d737a651367381239
lcubas <langelcubas9@gmail.com>
2024-10-31 22:18:28 -0500

    changed port of ms-order

--- apps/ms-order/src/main.ts
+++ apps/ms-order/src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(MsOrderModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get<number>('PORT', 3002));
+  await app.listen(configService.get<number>('PORT', 3000));
 }
 
 bootstrap();
